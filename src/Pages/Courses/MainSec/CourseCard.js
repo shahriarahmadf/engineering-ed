de694bedd1eafb7d2341ddeff6539d11a187f1eb
shahriarahmadf <shahriarahmadf@gmail.com>
2023-03-06 20:30:01 +0900

    linked course card to course details page

--- src/Pages/Courses/MainSec/CourseCard.js
+++ src/Pages/Courses/MainSec/CourseCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const CourseCard = ({course}) => {
-    const {course_code, course_details, course_image_url, credits, rating, title, total_students, instructor} = course;
+    const {course_code, course_id, course_image_url, credits, rating, title, total_students, instructor} = course;
     console.log(course_code);
     return (
         <div className='d-flex justify-content-center'>
-            <Card style={{ width: '28em' }}>
+            
+            <Link to={`/courses/${course_id}`} style={{color:'inherit', textDecoration:'none'}}><Card style={{ width: '28em' }}>
                 <Card.Img variant="top" src={course_image_url} />
                 <Card.Header>{course_code} | {credits} credits</Card.Header>
 
@@ -14,14 +16,14 @@ const CourseCard = ({course}) => {
                     <Card.Title>{title}</Card.Title>                            
                     <Card.Text>
                         Instructor: {instructor} <br></br>
-                        Course Rating: {rating}
+                        Course Rating: {rating} <br></br>
                     </Card.Text>
                 </Card.Body>
                 <Card.Body className='d-flex justify-content-around align-items-center'>
                     <Button variant="danger">Get Premium Access</Button>
                     <Card.Text><strong>{total_students} students enrolled</strong></Card.Text>
                 </Card.Body>
-            </Card>
+            </Card></Link>
         </div>
     );
 };
