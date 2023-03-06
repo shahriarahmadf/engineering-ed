77469f505c7fd890fe2303abc06f43890e715b91
shahriarahmadf <shahriarahmadf@gmail.com>
2023-03-06 19:17:26 +0900

    main section course card all loaded

--- src/Pages/Courses/MainSec/CourseCard.js
+++ src/Pages/Courses/MainSec/CourseCard.js
@@ -5,17 +5,21 @@ const CourseCard = ({course}) => {
     const {course_code, course_details, course_image_url, credits, rating, title, total_students, instructor} = course;
     console.log(course_code);
     return (
-        <div>
-            <Card style={{ width: '24em' }}>
+        <div className='d-flex justify-content-center'>
+            <Card style={{ width: '28em' }}>
                 <Card.Img variant="top" src={course_image_url} />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
+                <Card.Header>{course_code} | {credits} credits</Card.Header>
 
-                    <Button variant="primary">Go somewhere</Button>
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>                            
+                    <Card.Text>
+                        Instructor: {instructor} <br></br>
+                        Course Rating: {rating}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body className='d-flex justify-content-around'>
+                    <Button variant="danger">Get Premium Access</Button>
+                    <Card.Text><strong>{total_students} students enrolled</strong></Card.Text>
                 </Card.Body>
             </Card>
         </div>
--- src/Pages/Courses/MainSec/MainSec.js
+++ src/Pages/Courses/MainSec/MainSec.js
@@ -7,7 +7,7 @@ const MainSec = (props) => {
     return (
         <div className='mt-4 m-4'>
             <h2 className='text-center'>All Courses</h2>
-            <Row xs={1} md={2} lg={2} className="g-4">
+            <Row xs={1} md={2} lg={2} className="g-4 mt-4">
             {
                 courses.map(course => <CourseCard
                     key={course.course_id}
